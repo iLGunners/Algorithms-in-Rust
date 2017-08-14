a0b83bf46bdd4c9306dbbf123f8d739c5bdd4c82
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2017-08-14 13:36:06 +0600

    complete, locks at ackermann(4, 0)

--- number/ackermann-function/src/main.rs
+++ number/ackermann-function/src/main.rs
@@ -1,3 +1,17 @@
+fn ackermann(m: u64, n: u64) -> u64 {
+    match(m, n) {
+        (0, n) => n+1,
+        (m, 0) => ackermann(m-1, 1),
+        (m, n) => ackermann(m-1, ackermann(m, n-1))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("___ Ackermann Function ___");
+
+    for m in 0..5 {
+        for n in 0..(16-m) {
+            println!("ackermann({}, {}) = {}", m, n, ackermann(m, n));
+        }
+    }
 }
