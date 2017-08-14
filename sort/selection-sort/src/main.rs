28ca03ea35e53fe2b397dd8c649420d5b49eff5e
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2017-08-14 21:13:56 +0600

    Selection Sor complete

--- sort/selection-sort/src/main.rs
+++ sort/selection-sort/src/main.rs
@@ -1,3 +1,17 @@
+fn selection_sort(array: &mut [i32]) {
+    for i in 0..array.len() {
+        let min_index = (i..array.len()).min_by_key(|x| array[*x]).unwrap();
+        array.swap(min_index, i)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("___ Selection Sort ___");
+
+    let mut num_list = [ 5, 9, 8, 4, 1, 10, 7, 2, 3, 6, 0 ];
+    println!("unsorted list: {:?}", num_list);
+
+    selection_sort(&mut num_list);
+
+    println!("sorted list: {:?}", num_list);
 }
---
+++ sort/selection-sort/target/debug/selection-sort.d
@@ -0,0 +1,1 @@
+/home/noshu/code/rust-code/Algorithms-in-Rust/sort/selection-sort/target/debug/selection-sort: /home/noshu/code/rust-code/Algorithms-in-Rust/sort/selection-sort/src/main.rs
