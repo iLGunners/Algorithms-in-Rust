54eeb7a5129ecb9e61bb72825bd9505eafdb3d38
Mujtaba Nowshad <mujtabanowshad@gmail.com>
2017-08-27 19:24:35 +0600

    bubble sort complete

--- sort/bubble-sort/src/main.rs
+++ sort/bubble-sort/src/main.rs
@@ -1,6 +1,28 @@
+fn bubble_sort(array: &mut[i8]) {
+    let mut array_length = array.len();
+    let mut swapped_flag = true;
+ 
+    while swapped_flag {
+        swapped_flag = false;
+ 
+        for i in 1..array_length {
+            if array[i - 1] > array[i] {
+                array.swap(i - 1, i);
+                swapped_flag = true;
+            }
+        }
+ 
+        array_length = array_length - 1;
+    }
+}
+
 fn main() {
     println!("___ Bubble Sort ___");
 
     let mut num_list = [ 5, 9, 8, 4, 1, 10, 7, 2, 3, 6, 0 ];
     println!("unsorted list: {:?}", num_list);
+
+    bubble_sort(&mut num_list);
+
+    println!("sorted list: {:?}", num_list);
 }
---
+++ sort/bubble-sort/target/debug/bubble-sort.d
@@ -0,0 +1,1 @@
+/home/noshu/code/rust-code/Algorithms-in-Rust/sort/bubble-sort/target/debug/bubble-sort: /home/noshu/code/rust-code/Algorithms-in-Rust/sort/bubble-sort/src/main.rs
